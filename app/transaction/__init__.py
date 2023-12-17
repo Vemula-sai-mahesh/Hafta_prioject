41ea97b68bb1169c99aa17b2a901a52d0f957e56
Divineleo2001 <divineleo20@gmail.com>
2023-12-17 20:59:56 +0530

    initial commit for front

--- app/transaction/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint 
-
-bp = Blueprint('transaction' , __name__ , template_folder='templates/transaction')
-
-from app.transaction import attendence , advance , salary_sheet , performance , quickinput
--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,9 +1,15 @@
-const config= {
-  static_path: process.env.NODE_ENV !== 'production' ? 'http://127.0.0.1:5234' : 'http://api.jaitexart.org/static',
-  static_base: process.env.NODE_ENV !== 'production' ? 'http://127.0.0.1:5234/static/uploads' : 'https://saas-space.ams3.cdn.digitaloceanspaces.com/',
-  apiserver: process.env.NODE_ENV !== 'production' ? '127.0.0.1' : 'h-api.jaitexart.org',
-  apiserver_port: process.env.NODE_ENV !== 'production' ? '5234' : '80',
-}
+const config = {
+  static_path:
+    process.env.NODE_ENV !== "production"
+      ? "http://127.0.0.1:3000"
+      : "http://api.jaitexart.org/static",
+  static_base:
+    process.env.NODE_ENV !== "production"
+      ? "http://127.0.0.1:3000/static/uploads"
+      : "https://saas-space.ams3.cdn.digitaloceanspaces.com/",
+  apiserver: process.env.NODE_ENV !== "production" ? "127.0.0.1" : "127.0.0.1",
+  apiserver_port: process.env.NODE_ENV !== "production" ? "3000" : "80",
+};
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
@@ -11,21 +17,29 @@ export default {
     static_path: config.static_path,
     static_base: config.static_base,
   },
- 
+
   head: {
-    title: 'Hafta',
+    title: "Hafta",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/Logo-b.svg' },
-      { rel: 'preconnect',  href: 'https://fonts.gstatic.com' },
-      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap' },
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/Logo-b.svg" },
+      { rel: "preconnect", href: "https://fonts.gstatic.com" },
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
+      },
+    ],
   },
-  ssr:false,
+  ssr: false,
   // head: {
   //   title: 'front',
   //   htmlAttrs: {
@@ -42,13 +56,10 @@ export default {
   // },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/extras.js','~/plugins/inter.js'
-  ],
+  plugins: ["~/plugins/extras.js", "~/plugins/inter.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -56,17 +67,15 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // '@nuxtjs/html-validator'
-
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/buefy
-    ['nuxt-buefy'],
+    ["nuxt-buefy"],
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/auth',
-
+    "@nuxtjs/axios",
+    "@nuxtjs/auth",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -80,29 +89,28 @@ export default {
         //   callback: '/auth'
         // },
         redirect: {
-          login: '/auth',
-          logout: '/',
-          callback: '/auth',
-          home: '/'
+          login: "/auth",
+          logout: "/",
+          callback: "/auth",
+          home: "/",
         },
         endpoints: {
-          login: { url: '/auth/login', method: 'post', propertyName: 'token' },
-          logout: { url: '/auth/logout', method: 'delete' },
-          user: { url: '/auth/user', method: 'get', propertyName: 'user' }
+          login: { url: "/auth/login", method: "post", propertyName: "token" },
+          logout: { url: "/auth/logout", method: "delete" },
+          user: { url: "/auth/user", method: "get", propertyName: "user" },
         },
         // tokenRequired: true,
         // tokenType: '',
         // globalToken: true,
         // autoFetchUser: true
-      }
-    }
+      },
+    },
   },
   axios: {
-    host:  config.apiserver,
-    port:  config.apiserver_port,
+    host: config.apiserver,
+    port: config.apiserver_port,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
