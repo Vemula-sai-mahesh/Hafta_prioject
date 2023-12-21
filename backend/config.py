37cb8073db0d31df466a0dbf15eb3f827c122a98
VEMULA SAI MAHESH <125812065+Vemula-sai-mahesh@users.noreply.github.com>
2023-12-21 11:03:15 +0530

    Successfully connected backend to database

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,7 @@
-
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 class Config(object):
+    
+
     SECRET_KEY = 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@db:3306/hafta'
     print("connected")
@@ -15,3 +17,32 @@ class Config(object):
     # LANGUAGES = ['en', 'es']
     # MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     # POSTS_PER_PAGE = 25
+    
+# Replace 'mysql://user:password@localhost/database' with your MySQL connection details
+    db_url = 'mysql+pymysql://user:password@localhost/database'
+    engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)  # Set echo to True for debugging (prints SQL statements)
+
+    # Create a metadata instance
+    metadata = MetaData()
+
+    # Define the 'users' table
+    users_table = Table(
+        'users',
+        metadata,
+        Column('id', Integer, primary_key=True),
+        Column('name', String(50)),
+        Column('age', Integer)
+    )
+
+    # Create the table in the database
+    metadata.create_all(engine)
+
+    # Perform database operations using the engine
+    with engine.connect() as connection:
+        # Example: Inserting a new user
+        connection.execute(users_table.insert().values(name='Jane Doe', age=30))
+
+        # Example: Querying users
+        result = connection.execute(users_table.select())
+        for row in result:
+            print(f"User ID: {row['id']}, Name: {row['name']}, Age: {row['age']}")
